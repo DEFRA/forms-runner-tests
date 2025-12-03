50ff6da390b56a047f40ac893ed943e0c184429e
Parvesh M <parvesh.malhotra@checkatrade.com>
2025-12-03 12:28:12 +0000

    feat: initial playwright scaffolding

---
+++ .prettierrc.js
@@ -0,0 +1,29 @@
+/**
+ * Prettier config
+ * @type {Config}
+ */
+export default {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "none",
+  overrides: [
+    {
+      files: "*.md",
+      options: {
+        embeddedLanguageFormatting: "off",
+        singleQuote: false,
+      },
+    },
+    {
+      files: "*.scss",
+      options: {
+        printWidth: 120,
+        singleQuote: false,
+      },
+    },
+  ],
+};
+
+/**
+ * @import { Config } from 'prettier'
+ */
